9211abfbd5fa89dc3567561e5a2cc21e77bf876a
EfrenLlanes17 <151380749+EfrenLlanes17@users.noreply.github.com>
2025-10-01 12:12:11 -0500

    Create 1518_numWaterBottles.cpp

---
+++ C++/1518_numWaterBottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int drankTotal = numBottles;
+        int numEmpty = numBottles;
+        while(numEmpty >= numExchange){
+            drankTotal += numEmpty / numExchange;
+            numEmpty = (numEmpty % numExchange) + (numEmpty / numExchange);
+        }
+            
+        return drankTotal;
+    }
+};
