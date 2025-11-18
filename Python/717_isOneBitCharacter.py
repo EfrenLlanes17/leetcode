a6faddaba1da95bbd959344c1cb3dd39ae89b262
EfrenLlanes17 <151380749+EfrenLlanes17@users.noreply.github.com>
2025-11-18 11:53:36 -0600

    Create 717_isOneBitCharacter.py

---
+++ Python/717_isOneBitCharacter.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        while i < len(bits)-1:
+            if bits[i] == 1:
+                i += 1
+            i+=1
+        if len(bits)-1 == i:
+                return True        
+        return False
+            
